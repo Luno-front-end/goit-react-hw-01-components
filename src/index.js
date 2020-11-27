37657b469fcbf87ef7c3db1b82a94f868dee7e6a
Luno-front-end <63469409+Luno-front-end@users.noreply.github.com>
2020-11-27 19:12:32 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App.js'
 import 'modern-normalize/modern-normalize.css';
-import './main.css'
 
 
 ReactDom.render(<App />, document.querySelector('#root'))
